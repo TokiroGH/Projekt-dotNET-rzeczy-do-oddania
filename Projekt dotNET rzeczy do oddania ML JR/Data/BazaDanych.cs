9e3fe8235f5de633fe7c959246f3895ad6bfebf8
johnyrosnki <jassko1509@gmail.com>
2022-05-14 16:27:28 +0200

    Testowanie

---
+++ Projekt dotNET rzeczy do oddania ML JR/Data/BazaDanych.cs
@@ -0,0 +1,13 @@
+﻿
+ using Projekt_dotNET_rzeczy_do_oddania_ML_JR.Models;
+using Microsoft.EntityFrameworkCore;
+
+ 
+        public class BazaDanych : DbContext
+        {
+    public BazaDanych(DbContextOptions options) : base(options) { }
+
+    public DbSet<Dane> Dane { get; set; }
+        }
+    
+
---
+++ Projekt dotNET rzeczy do oddania ML JR/Models/Dane.cs
@@ -0,0 +1,10 @@
+﻿namespace Projekt_dotNET_rzeczy_do_oddania_ML_JR.Models
+{
+    public class Dane
+
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
